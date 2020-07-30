5484a2e41ae35c9934bb6ff37ec1682d1a73a799
dmitry barinov <barinov3d@gmail.com>
2020-07-30 13:18:44 +0300

    Initial commit

---
+++ src/test/java/ru/commands/ui/demo5Tests0.java
@@ -0,0 +1,51 @@
+package ru.commands.ui;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.open;
+
+@Tag("group5")
+public class demo5Tests0 {
+    private final static String protocol = "http://";
+
+    @BeforeAll
+    public static void setUp() {
+        //Url удалённого веб драйвера
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        //Определяем какой браузер будем использовать
+        Configuration.browser = "chrome";
+        //Размер окна браузера
+        Configuration.browserSize = "1920x1080";
+        //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //Включить поддержку отображения экрана браузера во время выполнения теста
+        capabilities.setCapability("enableVNC", true);
+        //Включение записи видео в процессе выполнения тестов
+        capabilities.setCapability("enableVideo", false);
+        //Переопределяем Browser capabilities
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"ya.ru", "google.com"})
+    public void openPage(String path) {
+        open(protocol + path);
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"yahoo.com", "vk.com"})
+    public void openPage2(String path) {
+        open(protocol + path);
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"mvnrepository.com", "junit.org"})
+    public void openPage3(String path) {
+        open(protocol + path);
+    }
+}
