d024906426aeba38e5db777f73837aafcac5a48f
dmitry barinov <barinov3d@gmail.com>
2020-10-14 13:44:19 +0300

    Update

---
+++ src/test/java/ru/commands/ui/CommonTests.java
@@ -0,0 +1,50 @@
+package ru.commands.ui;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
+
+public class CommonTests {
+
+    /*    @BeforeEach
+    public static void setUp() throws MalformedURLException {
+        //Url удалённого веб драйвера
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        //Определяем какой браузер будем использовать
+        Configuration.browser = "chrome";
+        //Размер окна браузера
+        Configuration.browserSize = "1920x1080";
+        //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //Включить поддержку отображения экрана браузера во время выполнения теста
+        capabilities.setCapability("enableVNC", true);
+        //Включение записи видео в процессе выполнения тестов
+        capabilities.setCapability("enableVideo", false);
+        //Переопределяем Browser capabilities
+        capabilities.setBrowserName("chrome");
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide().enableLogs(LogType.BROWSER, Level.ALL).screenshots(true).savePageSource(false));
+    }*/
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        final String url = "http://localhost:4444/wd/hub";
+        WebDriver driver = new RemoteWebDriver(new URL(url), DesiredCapabilities.chrome());
+        driver.manage().window().setSize(new Dimension(1920,1024));
+        WebDriverRunner.setWebDriver(driver);
+    }
+
+    @AfterEach
+    public void stopDriver() {
+        Optional.ofNullable(WebDriverRunner.getWebDriver()).ifPresent(WebDriver::quit);
+    }
+}
--- src/test/java/ru/commands/ui/demo2Tests.java
+++ src/test/java/ru/commands/ui/demo2Tests.java
@@ -19,36 +19,8 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 @Tag("group2")
-public class demo2Tests {
+public class demo2Tests extends CommonTests {
     private final static String protocol = "http://";
-
-/*    @BeforeEach
-    public static void setUp() throws MalformedURLException {
-        //Url удалённого веб драйвера
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        //Определяем какой браузер будем использовать
-        Configuration.browser = "chrome";
-        //Размер окна браузера
-        Configuration.browserSize = "1920x1080";
-        //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        //Включить поддержку отображения экрана браузера во время выполнения теста
-        capabilities.setCapability("enableVNC", true);
-        //Включение записи видео в процессе выполнения тестов
-        capabilities.setCapability("enableVideo", false);
-        //Переопределяем Browser capabilities
-        capabilities.setBrowserName("chrome");
-        Configuration.browserCapabilities = capabilities;
-        SelenideLogger.addListener("AllureSelenide",
-                new AllureSelenide().enableLogs(LogType.BROWSER, Level.ALL).screenshots(true).savePageSource(false));
-    }*/
-    @BeforeEach
-    public void setUp() throws MalformedURLException {
-        final String url = "http://172.17.0.3:4444/wd/hub";
-        WebDriver driver = new RemoteWebDriver(new URL(url), DesiredCapabilities.chrome());
-        driver.manage().window().setSize(new Dimension(1920,1024));
-        WebDriverRunner.setWebDriver(driver);
-    }
 
     @ParameterizedTest()
     @ValueSource(strings = {"google.com"})
@@ -88,9 +60,5 @@ public class demo2Tests {
             $x("//a[@id='issue_12_link']").click();
             $x("//h1[contains(@class, 'gh-header-title')]").should(Condition.text("Hello, World!"));
         });
-    }
-    @AfterEach
-    public void stopDriver() {
-        Optional.ofNullable(WebDriverRunner.getWebDriver()).ifPresent(WebDriver::quit);
     }
 }
